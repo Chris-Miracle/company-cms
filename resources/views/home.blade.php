c4e3ee7ccc57451e1c33ce09fb197fa8ee5e877f
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2022-03-27 19:18:41 +0100

    design ui for admin page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,56 +2,36 @@
 
 @section('content')
 <div class="container">
-     <div class="row justify-content-center">
+    <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Add New Movies') }}</div>
+            <div class="jumbotron">
+                <h1 class="display-4">Welcome</h1>
+                <p class="lead">This is a simple Employee Management System, you can manage your company employee here.</p>
+                <hr class="my-4">
+                <p>Start by creating new Employee with the button below.</p>
+                <a class="btn btn-primary btn-lg" href="{{route('create-employee')}}" role="button">Create Employee</a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-white bg-dark mb-3" style="">
+                <div class="card-header">Wallet</div>
                 <div class="card-body">
-                    <form  method="post" enctype="multipart/form-data">
-                        @csrf
-                        @method('post')
-                        <div class="row mb-4">
-                            <div class="form-group col-md-6 mb-1">
-                                <label for="cinemaLocation">Cinema Location</label>
-                                <select class="form-control @error('cinema_location') is-invalid @enderror" name="cinema_location">
-                                    <option value=""> Select Cinema Location</option>
-                                    {{-- @foreach ($locations as $cinema )
-                                        <option value="{{$cinema->id}}">{{ $cinema->location }}</option>
-                                    @endforeach --}}
-                                </select>
-                                <div class="text-danger">{{$errors->first('cinema_location')}}</div>
-                            </div>
-
-                            <div class="form-group col-md-6 mb-1">
-                                <label for="movieTitle">Title</label>
-                                <input type="text" class="form-control @error('movie_title') is-invalid @enderror" placeholder="Eg: Spider Man far from home" name="movie_title" value="{{old('movie_title')}}">
-                                <div class="text-danger">{{$errors->first('movie_title')}}</div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-4">
-                            <div class="form-group col-md-6 mb-1">
-                                <label for="showDate">Show Date</label>
-                                <input type="date" class="form-control @error('show_date') is-invalid @enderror" name="show_date" value="{{old('show_date')}}">
-                                <div class="text-danger">{{$errors->first('show_date')}}</div>
-                            </div>
-
-                            <div class="form-group col-md-6 mb-1">
-                                <label for="showTime">Show Time</label>
-                                <input type="time" class="form-control @error('show_time') is-invalid @enderror" name="show_time" value="{{old('show_time')}}">
-                                <div class="text-danger">{{$errors->first('show_time')}}</div>
-                            </div>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary mt-1">Upload Movie</button>
-                    </form>
+                    <h5 class="card-title">250,000,000</h5>
+                    <p class="card-text">This is your current wallet balance.</p>
+                </div>
+            </div>
+            <div class="card border-secondary mb-3" style="">
+                <div class="card-header">Employee Summary</div>
+                <div class="card-body text-secondary">
+                    <h5 class="card-title">4 Employees</h5>
+                    <p class="card-text">This is the total number of active employee you currently have registered on your system.</p>
                 </div>
             </div>
         </div>
 
         <div class="col-md-12 mt-5">
             <div class="section-title text-center">
-                <h3>Movie List</h3>
+                <h3>Employee List</h3>
             </div>
             <table class="table">
                 <thead>
